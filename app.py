c2f53ce7b0e8c343cbb93273ef9d4ead5a514d18
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2023-11-20 11:36:48 +0000

    Add example to debug

---
+++ app.py
@@ -0,0 +1,19 @@
+from debuggable import reverse_it, upcase_it, add_numbers_to_end, add_numbers_to_start
+
+reversed = reverse_it('you are an amazing developer')
+
+upcased = upcase_it(reversed)
+
+number_to_end = add_numbers_to_end(upcased)
+
+number_to_start = add_numbers_to_start(number_to_end)
+
+print(number_to_start)
+
+# 'string' => reverse_it() function
+# upcase_it()
+# add_numbers_to_end()
+# add_number_to_start()
+# printed
+
+# string.capitaze().reverse()
---
+++ debuggable.py
@@ -0,0 +1,12 @@
+def reverse_it(string):
+    return string[::-1]
+
+def upcase_it(string):
+    return string.capitalize()
+
+def add_numbers_to_start(string):
+    return str(13) + string
+
+def add_numbers_to_end(string):
+    return string + str(23)
+
